cf3b77d4ee84340ac7a2af181a71fc7a0b5d5604
Jeff Gradner <gradnerj@gmail.com>
2020-09-01 10:50:31 -0600

    Add project files.

---
+++ FoodDelivery/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FoodDelivery.Models;
+
+namespace FoodDelivery.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FoodDelivery.Models.Category> Category { get; set; }
+    }
+}
---
+++ FoodDelivery/Pages/Admin/CategoryIndex.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FoodDelivery.Data;
+using FoodDelivery.Models;
+
+namespace FoodDelivery.Pages.Admin
+{
+    public class CategoryIndexModel : PageModel
+    {
+        private readonly FoodDelivery.Data.ApplicationDbContext _context;
+
+        public CategoryIndexModel(FoodDelivery.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
