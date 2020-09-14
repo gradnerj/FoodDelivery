801c5dadc067928b5d8ac6c7727eab4641488240
Jeff Gradner <gradnerj@gmail.com>
2020-09-14 14:42:02 -0600

    7 of 7 videos week 3, everything looks good

--- FoodDelivery/Controllers/CategoryController.cs
+++ FoodDelivery/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using FoodDelivery.DataAccess.Data.Repository.IRepository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace FoodDelivery.Controllers {
     [Route("api/[controller]")]
@@ -23,5 +24,15 @@ namespace FoodDelivery.Controllers {
             return Json(new { data = _unitOfWork.Category.GetAll() });
         }
 
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            var objFromDb = _unitOfWork.Category.GetFirstorDefault(u => u.Id == id);
+            if(objFromDb == null) {
+                return Json(new { success = false, message = "Error while deleting"});
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
     }
 }
