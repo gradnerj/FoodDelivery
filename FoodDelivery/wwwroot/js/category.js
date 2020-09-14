c04ad74cd49b74b11fdf9d31770e584eb43220de
Jeff Gradner <gradnerj@gmail.com>
2020-09-14 14:24:07 -0600

    updates through video 6 of 7 week 3

--- FoodDelivery/wwwroot/js/category.js
+++ FoodDelivery/wwwroot/js/category.js
@@ -14,7 +14,17 @@ function loadList() {
         "columns": [
             { data: "name", width: "40%" },
             { data: "displayOrder", width: "30%" },
-            { data: "id", width: "30%" }
+            {
+                data: "id", width: "30%",
+                "render": function (data) {
+                    return `<div class="text-center">
+                            <a href="/Admin/Category/Upsert?id=${data}"
+                            class ="btn btn-success text-white style="cursor:pointer; width=100px;"> <i class="far fa-edit"></i>Edit</a>
+                            <a onClick=Delete('/api/category/'+${data})
+                            class ="btn btn-danger text-white style="cursor:pointer; width=100px;"> <i class="far fa-trash-alt"></i>Delete</a>
+                    </div>`;
+                }
+            }
         ],
         "language": {
             "emptyTable": "no data found."
