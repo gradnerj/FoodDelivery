de61fed0b72ef77081fcdacc3138cff9df5e8b08
Jeff <gradnerj@gmail.com>
2020-09-22 10:02:13 -0600

    Added MenuItem Controller

---
+++ FoodDelivery/Controllers/MenuItemController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using FoodDelivery.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostingEnv;
+        public MenuItemController(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnv)
+        {
+            _unitOfWork = unitOfWork;
+            _hostingEnv = hostingEnv;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.MenuItem.GetAll() });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.MenuItem.GetFirstorDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.MenuItem.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+    }
+}
