e854f42622fe619e8e4e633e0317ebad00adbf03
Jeff <gradnerj@gmail.com>
2020-09-22 10:15:32 -0600

    Added MenuItem Repo interface and class

---
+++ DataAccess/Data/Repository/IRepository/IMenuItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using FoodDelivery.DataAccess.IRepository;
+using FoodDelivery.Models.Models;
+
+namespace FoodDelivery.DataAccess.Data.Repository.IRepository {
+    public interface IMenuItemRepository : IRepository<MenuItem> {
+
+        void Update(MenuItem menuItem);
+
+    }
+}
---
+++ DataAccess/Data/Repository/MenuItemRepository.cs
@@ -0,0 +1,23 @@
+﻿using FoodDelivery.Data;
+using FoodDelivery.DataAccess.Data.Repository.IRepository;
+using FoodDelivery.DataAccess.IRepository;
+using FoodDelivery.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FoodDelivery.DataAccess.Data.Repository {
+    public class MenuItemRepository : Repository<MenuItem>, IMenuItemRepository {
+        private readonly ApplicationDbContext _context;
+        public MenuItemRepository(ApplicationDbContext context) : base(context) {
+            _context = context;
+        }
+
+
+        public void Update(MenuItem menuItem) {
+            var menuItemObj = _context.MenuItem.FirstOrDefault(m => m.Id == menuItem.Id);
+        }
+
+    }
+}
