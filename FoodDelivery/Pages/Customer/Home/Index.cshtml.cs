feb2e22208a054db0bd3713114ebcc8e6e417575
Jeff <gradnerj@gmail.com>
2020-09-24 13:40:42 -0600

    Added Customer Home Page

---
+++ FoodDelivery/Pages/Customer/Home/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FoodDelivery.DataAccess.Data.Repository.IRepository;
+using FoodDelivery.Models;
+using FoodDelivery.Models.Models;
+
+namespace FoodDelivery.Pages.Customer.Home {
+    public class IndexModel : PageModel { 
+        private readonly IUnitOfWork _unitOfWork; 
+        public IndexModel(IUnitOfWork unitOfWork) { 
+            _unitOfWork = unitOfWork; 
+        } 
+        public IEnumerable<MenuItem> MenuItemList { get; set; } 
+        public IEnumerable<Category> CategoryList { get; set; } 
+        public void OnGet() { 
+            MenuItemList = _unitOfWork.MenuItem.GetAll(null, null, "Category,FoodType"); 
+            CategoryList = _unitOfWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null); 
+        } 
+    } 
+}
--- FoodDelivery/Pages/Index.cshtml.cs
+++ FoodDelivery/Pages/Index.cshtml.cs
@@ -4,18 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-
-namespace FoodDelivery.Pages {
-    public class IndexModel : PageModel {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger) {
-            _logger = logger;
-        }
-
-        public void OnGet() {
-
-        }
-    }
+namespace FoodDelivery.Pages { 
+    public class IndexModel : PageModel { 
+        public IActionResult OnGet() { 
+            return RedirectToPage("/Customer/Home/Index"); 
+        } 
+    } 
 }
