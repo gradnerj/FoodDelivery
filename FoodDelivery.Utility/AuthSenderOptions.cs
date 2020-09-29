33bdc152fe415689871450ed32a5fde3da9a6221
Jeff <gradnerj@gmail.com>
2020-09-29 11:27:43 -0600

    Added AuthSenderOptions, EmailSender, and SD for roles

---
+++ FoodDelivery.Utility/AuthSenderOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodDelivery.Utility {
+    public class AuthSenderOptions {
+        private string user = "Food Delivery"; // The name you want to show up on your email
+         // Make sure the string passed in below matches your API Key
+        private string key = Environment.GetEnvironmentVariable("SENDGRID");
+        public string SendGridUser { get { return user; } }
+        public string SendGridKey { get { return key; } }
+    }
+}
+
---
+++ FoodDelivery.Utility/SD.cs
@@ -0,0 +1,6 @@
+﻿public class SD {
+    public const string ManagerRole = "Manager";
+    public const string DriverRole = "Driver";
+    public const string KitchenRole = "Kitchen";
+    public const string CustomerRole = "Customer";
+}
