74c8bd65de25983c48089ca163b365f6d24318b9
Jeff <gradnerj@gmail.com>
2020-09-29 11:28:26 -0600

    Added ApplicationUser class

---
+++ FoodDelivery.Models/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class ApplicationUser : IdentityUser {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    [NotMapped]
+    public string FullName { get { return FirstName + " " + LastName; } }
+}
