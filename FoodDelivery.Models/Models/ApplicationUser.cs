fa0da3fca00e8bc19d8d86fa146251e70ee4da08
Jeff <gradnerj@gmail.com>
2020-10-01 10:51:22 -0600

    Added namespace to ApplicationUser model

--- FoodDelivery.Models/Models/ApplicationUser.cs
+++ FoodDelivery.Models/Models/ApplicationUser.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class ApplicationUser : IdentityUser {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    [NotMapped]
-    public string FullName { get { return FirstName + " " + LastName; } }
+namespace FoodDelivery.Models {
+    public class ApplicationUser : IdentityUser {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
 }
