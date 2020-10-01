0ecc3aae4de85e26063f390057826cdc6536f270
Jeff <gradnerj@gmail.com>
2020-10-01 10:47:14 -0600

    Added static details class for roles

--- FoodDelivery.Utility/SD.cs
+++ FoodDelivery.Utility/SD.cs
@@ -1,4 +1,5 @@
-﻿public class SD {
+﻿// SD == Static Details
+public class SD {
     public const string ManagerRole = "Manager";
     public const string DriverRole = "Driver";
     public const string KitchenRole = "Kitchen";
