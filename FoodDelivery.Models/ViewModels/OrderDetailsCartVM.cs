1b39299bfcc44ab37029528b9ac3fc65eb744fa4
Jeff <gradnerj@gmail.com>
2020-10-13 10:06:49 -0600

    Added Cart Page and OrderDetails ViewModel

---
+++ FoodDelivery.Models/ViewModels/OrderDetailsCartVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodDelivery.Models.ViewModels {
+    public class OrderDetailsCartVM {
+        public OrderHeader OrderHeader { get; set; }
+        public List<ShoppingCart> ListCart { get; set; }
+    }
+}
---
+++ FoodDelivery/Pages/Customer/Cart/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FoodDelivery.DataAccess.Data.Repository.IRepository;
+using FoodDelivery.Models;
+using FoodDelivery.Models.ViewModels;
+using FoodDelivery.Utility;
+
+namespace FoodDelivery.Pages.Customer.Cart
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public OrderDetailsCartVM OrderDetailsCart { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+
+       
+    }
+}
