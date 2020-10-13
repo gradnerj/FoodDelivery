a25b4ad5034c1f041f3704e63f1183256e503a38
Jeff <gradnerj@gmail.com>
2020-10-13 09:46:06 -0600

    Added OrderDetails and OrderHeader

---
+++ FoodDelivery.Models/Models/OrderHeader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FoodDelivery.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        [Display(Name = "Order Total")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        [Display(Name ="Delivery Time")]
+        public DateTime DeliveryTime { get; set; }
+
+        [Required]
+        [NotMapped]
+        public DateTime DeliveryDate { get; set; }
+
+        public string Status { get; set; }
+        public string PaymentStatus { get; set; }
+        public string Comments { get; set; }
+
+        [Display(Name ="Delivery Name")]
+        public string DeliveryName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public string TransactionId { get; set; }
+
+        
+    }
+}
