3263a54da555b359a9ea27a92b117c82337cadb1
Jeff <gradnerj@gmail.com>
2020-11-04 21:10:38 -0700

    Added UpdateMenuItemTest

--- UnitTests/MenuItemTests.cs
+++ UnitTests/MenuItemTests.cs
@@ -1,8 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FoodDelivery.Data;
+using FoodDelivery.DataAccess.Data.Repository;
+using FoodDelivery.DataAccess.Data.Repository.IRepository;
+using FoodDelivery.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests {
-    class MenuItemTests {
+    [TestClass]
+    public class MenuItemTests {
+
+        private IConfigurationRoot _configuration;
+        private DbContextOptions<ApplicationDbContext> _options;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MenuItemTests() {
+            var builder = new ConfigurationBuilder().SetBasePath(System.IO.Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            _configuration = builder.Build();
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(_configuration.GetConnectionString("ApplicationDbContext")).Options;
+            var context = new ApplicationDbContext(_options);
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        [TestMethod]
+        public void UpdateMenuItemTest() {
+            MenuItem m1 = new MenuItem() {
+                Name = "TestMenuItem",
+                Description = "Test Description",
+                Image = "",
+                Price = (float)5.5,
+                CategoryId = 5,
+                FoodTypeId = 5,
+            };
+
+            _unitOfWork.MenuItem.Add(m1);
+            _unitOfWork.Save();
+
+            m1.Price = (float)6.66;
+
+            _unitOfWork.MenuItem.Update(m1);
+            _unitOfWork.Save();
+
+            var objFromDb = _unitOfWork.MenuItem.GetFirstorDefault(m => m.Name == "TestMenuItem");
+
+            Assert.IsTrue(objFromDb.Price == (float)6.66);
+
+        }
     }
 }
